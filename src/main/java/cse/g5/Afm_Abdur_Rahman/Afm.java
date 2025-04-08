02f1f624c5bc072ce6baa0c22e4dafff88bc6b14
Afm <afmabdur2@gmail.com>
2025-04-08 09:17:12 +0600

    initialized

---
+++ src/main/java/cse/g5/Afm_Abdur_Rahman/Afm.java
@@ -0,0 +1,15 @@
+package cse.g5.Afm_Abdur_Rahman;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Afm extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
