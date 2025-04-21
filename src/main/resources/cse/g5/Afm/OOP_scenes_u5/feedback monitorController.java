14a047711f0c89d3837e73d4008af00ea671283b
Afm Abdur Rahman <afmabdur2@gmail.com>
2025-04-21 22:57:17 +0600

    java

---
+++ src/main/resources/cse/g5/Afm/OOP_scenes_u5/feedback monitorController.java
@@ -0,0 +1,16 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Button analyzefeedbackbutton;
+
+    @FXML
+    private TextArea feedbacklogtextarea;
+
+    @FXML
+    private Button generatetrendsummaryoffeedbackbutton;
+
+}
