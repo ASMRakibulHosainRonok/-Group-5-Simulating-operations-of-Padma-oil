f61f17e69ce2c8ef6b4f4d348534fe466c57d618
ronok <ronokhossain1@gmail.com>
2025-04-21 22:59:11 +0600

    controllers update

---
+++ src/main/resources/cse/g5/Ronok/Customer/delivery_updatesController.java
@@ -0,0 +1,50 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private TableView<?> delivery_updates_table;
+
+    @FXML
+    void check_prices(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delivery_update(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void order_confirmation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void place_order(ActionEvent event) {
+
+    }
+
+    @FXML
+    void raise_complaint(ActionEvent event) {
+
+    }
+
+    @FXML
+    void refesh(ActionEvent event) {
+
+    }
+
+    @FXML
+    void track_order(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/resources/cse/g5/Ronok/Customer/place_orderController.java
@@ -0,0 +1,64 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private DatePicker date;
+
+    @FXML
+    private TableView<?> oil_detail_table;
+
+    @FXML
+    void approval_notification(ActionEvent event) {
+
+    }
+
+    @FXML
+    void check_prices(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delivery_updates(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void oil_name(ActionEvent event) {
+
+    }
+
+    @FXML
+    void oil_quantity(ActionEvent event) {
+
+    }
+
+    @FXML
+    void order_confirmation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void place_order(ActionEvent event) {
+
+    }
+
+    @FXML
+    void raise_complaints(ActionEvent event) {
+
+    }
+
+    @FXML
+    void track_order(ActionEvent event) {
+
+    }
+
+}
