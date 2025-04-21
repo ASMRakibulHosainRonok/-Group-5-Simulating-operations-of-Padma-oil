eb17e9d38761247d62026f9e6a028772943a5f90
Afm Abdur Rahman <afmabdur2@gmail.com>
2025-04-22 00:34:00 +0600

    fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,10 @@ module cse.g5 {
     requires javafx.fxml;
     requires java.desktop;
 
+    exports cse.g5;
+    exports cse.g5.Afm;
 
     opens cse.g5 to javafx.fxml;
+    opens cse.g5.Afm to javafx.fxml, javafx.graphics;
     opens cse.g5.Shemanto.oop_scenes to javafx.fxml;
-    exports cse.g5;
 }
